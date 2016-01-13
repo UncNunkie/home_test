88776f2e76fefbe78525503ce9c670d68831ac39
Aleksey Trifonov <trifonov@corp.sputnik.ru>
2016-01-13 19:39:48 +0300

    added url namspaces for templates, removed try-catch for uestions details

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,7 +2,7 @@
 
 from django.http import Http404
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
@@ -14,11 +14,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    print question
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/details.html', {'question': question})
 
 
